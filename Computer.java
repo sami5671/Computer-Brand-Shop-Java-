ddc934bea0959f9fbae2fd2203afa725a71e9862
sami5671 <samialam5671@gmail.com>
2023-11-18 00:49:46 +0600

    .

---
+++ Computer.java
@@ -0,0 +1,10 @@
+
+
+abstract public class Computer {
+    
+    protected String brandName;
+    protected String features;
+    
+    abstract int calculation(int originalPrice, int discount);
+
+}
